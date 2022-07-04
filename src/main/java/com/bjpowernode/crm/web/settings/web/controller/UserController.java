cac693bb62455b02c329f1849f5f0b0d16d2a192
BigRunFaFishKiller <2214016630@qq.com>
2022-07-04 22:05:38 +0800

    首页跳转至登录页面的实现

---
+++ src/main/java/com/bjpowernode/crm/web/settings/web/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.bjpowernode.crm.web.settings.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author:大润发杀鱼匠
+ * @Date:2022/7/4 21:47 crm-project
+ */
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/settings/qx/user/login")
+    public String toLogin() {
+        return "settings/qx/user/login";
+    }
+
+}
